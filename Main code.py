c73f9684d12b6db934c2bec04cae704b4ff8f9b7
mltrejos07-prog <mltrejos07@gmail.com>
2026-06-15 15:57:05 -0600

    codigo inicio

--- Main code.py
+++ Main code.py
@@ -1,2 +1,55 @@
 #Import
-import tkinter as Tk 
+import tkinter as tk
+from PIL import Image, ImageTk #para poder utilizar imagenes en el juego
+from tkinter import messagebox 
+
+
+
+#Constantes
+TAM = 40 # tam del pixel 
+
+
+matriz = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]         
+]   
+FILAS =len(matriz)
+COLUMNAS = len(matriz[0])
+
+
+
+
+
+#Funciones
+def menu_principal(): #fondo del menu
+    ventana.title("menu")
+    ventana.geometry("600x400")
+
+    img = Image.open("obj/menu.png")
+    img = img.resize((600, 400))
+
+    img_tk = ImageTk.PhotoImage(img)
+
+    label = tk.Label(ventana, image=img_tk)
+    label.image = img_tk  # importante
+    label.pack()
+
+#botones del menu
+    
+    
+
+
+#Ventana Principal 
+
+ventana = tk.Tk()
+menu_principal()
+
+ventana.mainloop()
