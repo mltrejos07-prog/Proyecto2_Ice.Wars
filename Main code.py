04a531e290bdbadb530f497dff40aa87e02d3a5b
EykonQR <e.quiros.5@estudiantec.cr>
2026-06-17 01:21:03 -0600

    avance 6.1 se agrego musica, se mejoro el sistema de guardado de info de los jugadorres para la clasificacion, se agrego los primeros pasos para la edicion de los mapas, se agrego el click en el mapa/canvas que hace que cambie de color(solo eso por el momento) 17/6/26

--- Main code.py
+++ Main code.py
@@ -1,11 +1,16 @@
 #Import
 import tkinter as tk
 from PIL import Image, ImageTk #para poder utilizar imagenes en el juego
-from tkinter import messagebox 
+from tkinter import messagebox
+from pygame import mixer
 
+#musica de fondo
+mixer.init()
+mixer.music.load("obj/musica.mp3")
+mixer.music.play(-1)
 
 #Constantes
-TAM = 75 # tam del pixel 
+TAM = 60 # tam del pixel 
 
 matriz = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  
@@ -20,7 +25,10 @@ matriz = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-]   
+]
+#Aqui se guardara la info de los jugadores para el ranking y el registro
+jugador1 = {}
+jugador2 = {}
 
 FILAS =len(matriz)
 COLUMNAS = len(matriz[0])
@@ -85,23 +93,58 @@ def abrir_jugar():
 
 #boton de "comenzar"
     def comenzar():
+        #validacion de los nombres de los jugadores
         if nombre_a.get() == "":
-            messagebox.showerror("ERROR", "Ingrese el nombre del atacante")
+            messagebox.showerror("ERROR","Ingrese el nombre del atacante")
             return
+        
         if nombre_d.get() == "":
-            messagebox.showerror("ERROR", "Ingrese el nombre del defensor")
-            return  
-        if faccion_atacante.get() == faccion_defensor.get(): 
-            messagebox.showerror("Error", "Las facciones deben ser diferentes") 
-            return 
-     
+            messagebox.showerror("ERROR","Ingrese el nombre del defensor")
+            return
+        
+        #Validar las contraseñas de los jugadores
+        if contra_a.get() == "":
+            messagebox.showerror("ERROR","Ingrese la contraseña del atacante")
+            return
+        
+        if contra_d.get() == "":
+            messagebox.showerror("ERROR","Ingrese la contraseña del defensor")
+            return
+        
+        #validar la seleccion de las facciones
+        if faccion_atacante.get() == "":
+            messagebox.showerror("ERROR","Seleccione la facción del atacante")
+            return
+        
+        if faccion_defensor.get() == "":
+            messagebox.showerror("ERROR","Seleccione la facción del defensor")
+            return
+        
+        #Info del jugador 1
+        jugador1["nombre"] = nombre_a.get()
+        jugador1["contraseña"] = contra_a.get()
+        jugador1["facción"] = faccion_atacante.get()
+
+        #Info del jugador 2
+        jugador2["nombre"] = nombre_d.get()
+        jugador2["contraseña"] = contra_d.get()
+        jugador2["facción"] = faccion_defensor.get()
+
+        print(jugador1)
+        print(jugador2)
+        
+        ventana_jugar.destroy() #cierra la ventana de inicio de sesion para abrir el juego
+
         abrir_editor_mapa()
+
     tk.Button(ventana_jugar, text="COMENZAR", command=comenzar).place(x=450, y=500)   
 def abrir_editor_mapa():
     editor = tk.Toplevel(ventana)
     editor.title("Editor de Mapa")
-    editor.geometry("1000x700")
-    
+    editor.geometry("1000x850")
+    tk.Label(editor, text="EDITOR DE MAPA").pack()
+
+   
 # Canvas donde se dibuja el mapa
     canvas = tk.Canvas(
         editor,
@@ -127,13 +170,39 @@ def abrir_editor_mapa():
                 y1,
                 x2,
                 y2,
-                outline="black")  
+                outline="black") 
+            
+        #esto es para detectar los clics
+    def click(event):
+
+
+
+        col = event.x // TAM
+        fila = event.y // TAM
+
+        matriz[fila][col] = 1
+
+        x1 = col * TAM
+        y1 = fila * TAM
+
+        x2 = x1 + TAM
+        y2 = y1 + TAM
+
+        canvas.create_rectangle(
+            x1,
+            y1,
+            x2,
+            y2,
+            fill="gray",
+            outline="black") 
+        
+    canvas.bind("<Button-1>", click)
 
 def abrir_top():
     ventana_top = tk.Toplevel(ventana)
     ventana_top.title("Top de jugadores")
     ventana_top.geometry("600x400")
-    tk.Label(ventana_top, text= "Ranking de Jugaores").pack(pady=20)
+    tk.Label(ventana_top, text= "Ranking de Jugadores").pack(pady=20)
 
 #Ventana Principal 
 
